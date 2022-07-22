bf146cb68f389b1f8fd8c9a9e4cfcd72550fa008
Tom Thornton <tombozwell@gmail.com>
2022-07-22 19:11:22 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(mjs|js|jsx)$/,
+        include: /node_modules/,
+        resolve: { mainFields: ["browser", "main"] },
+      },
+    ];
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
